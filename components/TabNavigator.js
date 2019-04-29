2ee75bf4d1120ca55ac8ac7aa245ef8728f708cb
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-04-29 14:36:30 +0300

    Add tab bar icons

--- components/TabNavigator.js
+++
@@ -1,8 +0,0 @@
-import { createBottomTabNavigator } from 'react-navigation';
-import HomeScreen from './HomeScreen';
-import PeopleScreen from './PeopleScreen';
-
-export default createBottomTabNavigator({
-  Home: HomeScreen,
-  People: PeopleScreen
-});
---
+++ components/TabNavigator.jsx
@@ -0,0 +1,33 @@
+/* eslint react/prop-types: off */
+
+import React from 'react';
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from 'react-navigation';
+import HomeScreen from './HomeScreen';
+import PeopleScreen from './PeopleScreen';
+import TabBarIcon from './TabBarIcon';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-home" />
+};
+
+const PeopleStack = createStackNavigator({
+  People: PeopleScreen
+});
+
+PeopleStack.navigationOptions = {
+  tabBarLabel: 'People',
+  tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-body" />
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  PeopleStack
+});
