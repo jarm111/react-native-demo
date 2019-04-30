570e2ba067def496e00a36a9230cd191d8575559
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-04-30 16:30:14 +0300

    Create content for HomeScreen

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
+import appIcon from '../assets/tgpa-icon.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,13 +8,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  icon: {
+    height: 100,
+    margin: 30,
+    width: 100
+  },
+  heading: {
+    fontSize: 26,
+    marginBottom: 10
+  },
+  description: {
+    fontSize: 12
   }
 });
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <Image source={appIcon} style={styles.icon} />
+      <Text style={styles.heading}>The Generic People App</Text>
+      <Text style={styles.description}>
+        The app displays data from Random User Generator Web API
+      </Text>
     </View>
   );
 };
