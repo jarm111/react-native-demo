0366d52761e9b361bb789388e0cd7194d01f7ab1
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-05-02 11:24:25 +0300

    Add flexDirection, wrapping and scrolling to PeopleScreen

--- components/PeopleScreen.jsx
+++ components/PeopleScreen.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Image, ScrollView } from 'react-native';
+import { StyleSheet, Image, ScrollView, View } from 'react-native';
 import placeHolderImage from '../assets/placeholder-image.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  image: {
+    margin: 5,
+    height: 100,
+    width: 100
+  },
+  scroll: {
+    flex: 1
   }
 });
 
@@ -13,10 +24,16 @@ const PeopleScreen = () => {
   const images = [];
 
   for (let i = 0; i < 20; i += 1) {
-    images[i] = <Image source={placeHolderImage} key={i} />;
+    images[i] = (
+      <Image source={placeHolderImage} key={i} style={styles.image} />
+    );
   }
 
-  return <ScrollView style={styles.container}>{images}</ScrollView>;
+  return (
+    <ScrollView style={styles.scroll}>
+      <View style={styles.container}>{images}</View>
+    </ScrollView>
+  );
 };
 
 export default PeopleScreen;
