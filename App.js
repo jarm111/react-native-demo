485306fd9070b780fbde38a25472dc49cfa39826
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-05-03 13:42:44 +0300

    Add network request

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-/* eslint react/jsx-filename-extension: "off" */
+/* eslint react/jsx-filename-extension: "off", no-console: "off" */
 
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import axios from 'axios';
 import AppNavigator from './components/AppNavigator';
 
 const styles = StyleSheet.create({
@@ -11,12 +12,33 @@ const styles = StyleSheet.create({
   }
 });
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <AppNavigator />
-    </View>
-  );
-};
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { people: {}, fetchDone: false };
+  }
 
-export default App;
+  componentDidMount() {
+    this.getPeople();
+  }
+
+  async getPeople() {
+    try {
+      const people = await axios.get('https://randomuser.me/api/?results=1');
+      this.setState({ people, fetchDone: true });
+    } catch (err) {
+      console.error('Error on fetch: ', err);
+    }
+  }
+
+  render() {
+    const { people, fetchDone } = this.state;
+    if (fetchDone) console.log(people.data.results[0].gender);
+
+    return (
+      <View style={styles.container}>
+        <AppNavigator />
+      </View>
+    );
+  }
+}
