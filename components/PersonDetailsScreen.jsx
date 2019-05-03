201602dcf10b93075393448aa256e7b2928c819b
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2019-05-03 10:28:54 +0300

    Create layout for PersonDetailsScreen

--- components/PersonDetailsScreen.jsx
+++ components/PersonDetailsScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import placeHolderImage from '../assets/placeholder-image.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,13 +8,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  image: {
+    margin: 10,
+    marginBottom: 30,
+    height: 150,
+    width: 150
+  },
+  info: {
+    fontSize: 20,
+    marginBottom: 15
   }
 });
 
 const PersonDetailsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>PersonDetailsScreen</Text>
+      <Image source={placeHolderImage} style={styles.image} />
+      <Text style={styles.info}>Name: Simon</Text>
+      <Text style={styles.info}>Age: 54</Text>
+      <Text style={styles.info}>Country: Spain</Text>
     </View>
   );
 };
